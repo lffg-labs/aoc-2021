08269902596c7f632e4cc61f7494a48d028821f0
Luiz Felipe Gonçalves <23662020+lffg@users.noreply.github.com>
2021-12-03 16:43:02 -0300

    Add template

---
+++ days/day-00/src/main.rs
@@ -0,0 +1,28 @@
+fn one(_input: &str) -> usize {
+    0
+}
+
+fn two(_input: &str) -> usize {
+    0
+}
+
+fn main() {
+    const INPUT: &str = include_str!("../input.txt");
+    println!("one = {}", one(INPUT));
+    println!("two = {}", two(INPUT));
+}
+
+#[cfg(test)]
+mod tests {
+    const TEST_INPUT: &str = include_str!("../test.txt");
+
+    #[test]
+    fn test_one() {
+        assert_eq!(super::one(TEST_INPUT), 7);
+    }
+
+    #[test]
+    fn test_two() {
+        assert_eq!(super::two(TEST_INPUT), 5);
+    }
+}
